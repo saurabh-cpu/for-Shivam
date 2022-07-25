f781b3a62dde2a429c7ef68cd12692e46ec6a76c
saurabh-cpu <57835026+saurabh-cpu@users.noreply.github.com>
2022-07-25 15:22:26 +0530

    https://github.com/saurabh-cpu/for-Shivam

--- app/src/main/java/com/example/bluetoothfinal/MassageScreen.kt
+++ app/src/main/java/com/example/bluetoothfinal/MassageScreen.kt
@@ -2,17 +2,14 @@ package com.example.bluetoothfinal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.os.Message
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.bluetoothfinal.databinding.ActivityMassageScreenBinding
-import java.util.*
+
 
 
 class MassageScreen : AppCompatActivity() {
 
-    lateinit var binding: ActivityMassageScreenBinding
+    private lateinit var binding: ActivityMassageScreenBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
